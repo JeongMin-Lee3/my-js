9148351620dd9d9545a517921247ee1f47490dcf
JeongMin-Lee3 <eorjarnl123@cu.ac.kr>
2026-06-22 17:34:46 +0900

    0622수업

---
+++ struc.js
@@ -0,0 +1,14 @@
+var user = {
+    email: "eorjarnl123@cu.ac.kr",
+    age: 20,
+    city: "Seoul"
+}
+
+var {email, age, city} = user;
+console.log(email, age, city);
+
+
+var fruits = ["apple", "banana", "cherry"];
+
+var [a, b, c] = fruits;
+console.log(a, b, c);
